0803fe190be398228fbb7b7c30fb71921f3de096
seannovela1 <seannovela5484@my.whccd.edu>
2024-10-26 08:02:53 -0700

    added javascript console.log

---
+++ script.js
@@ -0,0 +1,30 @@
+javascript
+// Write code to display the following message in the console:
+console.log("Hello World!");
+
+// Write code to concatenate three strings and display the result in the console:
+console.log("Hello" + " " + "World!");
+
+// Write code to display "Hello" on one line and "World!" on the next line in the console:
+console.log("Hello\nWorld!");
+
+// Write code to display the result of the arithmetic expression in the console:
+console.log(3 * 8 + 2);
+
+// Write code to display the result of the comparison:
+console.log(5 < 10);
+
+// Write code to display the result of the comparison:
+console.log(10 > 5);
+
+// Write code to display the result of the comparison:
+console.log("Apples" !== "Oranges");
+
+// Write code to display the result of the comparison:
+console.log(10 * 10 > 50 && 5 * 5 < 50);
+
+// Write code to display the result of the comparison:
+console.log(1 + 2 > 5 || 1 * 10 > 5);
+
+// Write code that demonstrates an automatic type conversion as shown in Chapter 01:
+console.log("5" - 3); // This shows 2 because "5" is automatically converted to a number.
